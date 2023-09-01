b42a95b1607b110712f71409db2392fac9d0d040
Gideon Anokye <gideonanokyebrown@gmail.com>
2023-09-01 10:11:25 +0000

    tomi pull this make I see

--- realtor_client/src/pages/Home.jsx
+++ realtor_client/src/pages/Home.jsx
@@ -5,12 +5,12 @@ import Header from "../components/Header";
 
 const Home = () => {
   return (
-    <div className=" h-screen   bg-cover bg-center" style={{ backgroundImage: "url('./heroimg.jpg')" }}>
+    <div className=" h-screen bg-cover bg-center  "  style={{  backgroundImage: "url('./heroimg.jpg')" }}>
         <Header/>
-        <div className="flex  justify-evenly items-center h-[500px] flex-col" >
+        <div className="flex  justify-evenly items-center h-[500px]  flex-col" >
         
-        <div className='flex flex-col self-center bg-[#287dcd1c] items-center justify-center '>
-              <h1 className='text-[18px]   md:text-[60px]  mt-4 text-center  text-[#fff]  font-bold '>
+        <div className='flex flex-col text-white bg-[#287dcd23] self-center items-center justify-center '>
+              <h1 className='text-[18px] relative z-50  md:text-[60px]  mt-4 text-center  text-[#fff]  font-bold '>
               We are here to make your dreams come true
               </h1>
             <span className='text-[18px] md:text-[40px] font-normal text-[#fff] self-center  '>Search for budget-friendly houses in your nearest location</span>
